d2a26ed4922c78195e9c19d07be51ad470a118e2
김석진 <69078017+eeptmxlsl15@users.noreply.github.com>
2024-07-07 23:30:54 +0900

    feat : button manager

--- Assets/KSJ/Scripts/ButtonContoller.cs
+++ Assets/KSJ/Scripts/ButtonContoller.cs
@@ -56,30 +56,6 @@ public class ButtonController : MonoBehaviour
         DataManager.Instance.InitializeData(desireStates);
     }
 
-    public void OnClick()
-    {
-		activeInventory = !activeInventory;
-        if (activeInventory)
-        {
-            backpack.transform.localScale = new Vector3(1f, 1f, 1f);
-			
-        }
-		else
-			backpack.transform.localScale = new Vector3(0f,0f,0f);
-
-		//인벤토리 열고 닫을 때 스크롤이 가장 밑에 있는 문제 해결
-		StartCoroutine(SetScrollbarValue());
-
-	}
-
-	private IEnumerator SetScrollbarValue()
-	{
-		// 1프레임 대기
-		yield return null;
-
-		// 1프레임 후에 scrollbar.value를 1로 설정
-		scrollbar.value = 1;
-	}
-
+	
 	
 }
--- Assets/KSJ/Scripts/ButtonManager.cs
+++ Assets/KSJ/Scripts/ButtonManager.cs
@@ -1,23 +1,38 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class ButtonManager : MonoBehaviour
 {
 	public List<Button> buttons; // 모든 버튼을 리스트로 관리
 	public List<GameObject> uiWindows; // 모든 UI 창을 리스트로 관리
-
 	private Dictionary<Button, GameObject> buttonToUIMap = new Dictionary<Button, GameObject>();
+
+	public List<Button> closeButtons;
+	private Dictionary<Button, GameObject> closeToUIMap = new Dictionary<Button, GameObject>();
 
 	private void Start()
 	{
+
 		// 버튼과 UI 창을 매핑
 		for (int i = 0; i < buttons.Count; i++)
 		{
-			buttonToUIMap.Add(buttons[i], uiWindows[i]);
-			buttons[i].onClick.AddListener(() => OnButtonClick(buttons[i]));
+			Button button = buttons[i];
+			GameObject uiWindow = uiWindows[i];
+			buttonToUIMap.Add(button, uiWindow);
+			button.onClick.AddListener(() => OnButtonClick(button));
 		}
+
+		// 클로즈 버튼과 UI 창을 매핑
+		for (int i = 0; i < closeButtons.Count; i++)
+		{
+			Button closeButton = closeButtons[i];
+			GameObject uiWindow = uiWindows[i];
+			closeToUIMap.Add(closeButton, uiWindow);
+			closeButton.onClick.AddListener(() => CloseButtonClick(closeButton));
+		}
+
 	}
 
 	private void OnButtonClick(Button clickedButton)
@@ -28,12 +43,28 @@ public class ButtonManager : MonoBehaviour
 		// 모든 UI 창을 숨김
 		foreach (var uiWindow in uiWindows)
 		{
-			uiWindow.transform.localScale = Vector3.zero;
+			uiWindow.transform.localScale = new Vector3(0,0,0);
 		}
 
+		
+
 		// 클릭된 버튼과 관련된 UI 창을 표시
-		clickedUIWindow.transform.localScale = Vector3.one;
+		clickedUIWindow.transform.localScale = new Vector3(1f,1f,1f);
+		//열리는 창에 스크롤바가 하나만 있다는 가정하에
+		
+		
+
 	}
 
+	private void CloseButtonClick(Button closeButton)
+	{
+		// 클릭된 버튼과 관련된 UI 창을 가져옴
+		GameObject closeUIWindow = closeToUIMap[closeButton];
+
+
+		// 클릭된 버튼과 관련된 UI 창을 표시
+		closeUIWindow.transform.localScale = new Vector3(0f, 0f, 0f);
+		//열리는 창에 스크롤바가 하나만 있다는 가정하에
+	}
 
 }
