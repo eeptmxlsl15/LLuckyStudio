8f25da2c3d54253d89bb5c4891f765a316f874e9
KANG SOO IN <ks00in213@gmail.com>
2024-07-09 20:20:20 +0900

    feat : Add Interactor

--- Assets/KSI/Scripts/Item/Interactor.cs
+++ Assets/KSI/Scripts/Item/Interactor.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Interactor : MonoBehaviour
 {
+	public int scoreValue = 10;
+
 	private void Interact()
 	{
-
+		ScoreManager.instance.AddScore(scoreValue);
+		Destroy(gameObject);
 	}
 
 	private void OnInteract()
 	{
 		Interact();
 	}
+
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			Interact();
+		}
+	}
+
 }
