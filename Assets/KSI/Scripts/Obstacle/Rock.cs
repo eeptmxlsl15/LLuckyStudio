0882b04354028c365083c5162c6fbc9ccc6ea5a5
KANG SOO IN <ks00in213@gmail.com>
2024-07-10 03:33:05 +0900

    feat : Add StonePillar

--- Assets/KSI/Scripts/Obstacle/Rock.cs
+++ Assets/KSI/Scripts/Obstacle/Rock.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 // 바위
 // 고정형 오브젝트(1단 점프)
+// 데미지 : -10
 public class Rock : Obstacle
 {
 	private void Start()
--- Assets/KSI/Scripts/Obstacle/StonePillar.cs
+++ Assets/KSI/Scripts/Obstacle/StonePillar.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 생선 가시
-// 날아오는 오브젝트
+// 돌기둥(2단 점프)
+// 고정형 오브젝트
 // 데미지 : -10
-public class FishThorn : Obstacle
+public class StonePillar : Obstacle
 {
-    private void Start()
-    {
+	private void Start()
+	{
 		damage = 10;
-    }
+	}
 }
