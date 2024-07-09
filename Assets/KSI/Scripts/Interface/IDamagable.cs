2f152a0167046906933cc84dbfba90d3f90cad1d
KANG SOO IN <ks00in213@gmail.com>
2024-07-10 03:22:41 +0900

    feat : Add Obstacle

--- Assets/KSI/Scripts/Interface/IDamagable.cs
+++ Assets/KSI/Scripts/Interface/IDamagable.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 
 public interface IDamagable
 {
-	void damage();
+	void TakeDamage(int damage);
 }
---
+++ Assets/KSI/Scripts/Obstacle/Obstacle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Obstacle : MonoBehaviour, IDamagable
+{
+	public int damage = 10;
+
+	public void TakeDamage(int damage) { }
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		IDamagable damagable = other.GetComponent<IDamagable>();
+		if (damagable != null)
+		{
+			damagable.TakeDamage(damage);
+			Destroy(gameObject);
+		}
+	}
+}
