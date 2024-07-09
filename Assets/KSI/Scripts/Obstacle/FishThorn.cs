53bddec4790611896b2c62f9c7abb95c09fb43d5
KANG SOO IN <ks00in213@gmail.com>
2024-07-10 03:26:02 +0900

    feat : Add FishThorn

---
+++ Assets/KSI/Scripts/Obstacle/FishThorn.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 날아오는 오브젝트
+// 생선 가시
+public class FishThorn : Obstacle
+{
+    private void Start()
+    {
+		damage = 10;
+    }
+}
