4fc50757a185bb95f683bd7d391a3579c2c188b9
KANG SOO IN <ks00in213@gmail.com>
2024-07-10 19:45:51 +0900

    feat : Add FixedPlatform

---
+++ Assets/KSI/Scripts/Platform/FixedPlatform.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 고정형 발판
+// 스토리 및 무한 모드
+public class FixedPlatform : Platform
+{
+	public override void Pass() 
+	{ 
+		// TODO : 통과
+	}
+}
