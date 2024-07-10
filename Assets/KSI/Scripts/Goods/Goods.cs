eba9bf388dbab389f4f81e0d316dc383de4bc0ab
KANG SOO IN <ks00in213@gmail.com>
2024-07-10 18:40:20 +0900

    fix : Change Item Into Goods

--- Assets/KSI/Scripts/Goods/Goods.cs
+++ Assets/KSI/Scripts/Goods/Goods.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Item : MonoBehaviour
+public abstract class Goods : MonoBehaviour
 {
 	[SerializeField] private float speed = 1f;
 
--- Assets/KSI/Scripts/Goods/JellyCoin.cs
+++ Assets/KSI/Scripts/Goods/JellyCoin.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 통조림(강화 재화)
-public class CannedFood : Item
-{
+//젤리 코인(점수 코인)
+public class JellyCoin : Goods
+{ 
 	private IScore scoreAdapter;
 
 	private void Awake()
-	{
-		scoreAdapter = new CannedFoodAdapter();
+    {
+		scoreAdapter = new JellyCoinAdapter();
 		// TODO : AudioClip 초기화
 	}
 
 	public override void Contact()
 	{
 		gameObject.SetActive(false);
-		scoreAdapter.AddScore(20);
+		scoreAdapter.AddScore(10);
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
