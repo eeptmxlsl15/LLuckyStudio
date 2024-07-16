1306dfa5d15ffd29245f1fc52249c6149b52dfef
KANG SOO IN <ks00in213@gmail.com>
2024-07-16 19:02:30 +0900

    fix : Edit SoleJelly

--- Assets/KSI/Scripts/Goods/SoleJelly.cs
+++ Assets/KSI/Scripts/Goods/SoleJelly.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 젤리 코인(점수 코인)
+// 젤리 발바닥(점수 코인)
 // 스토리 및 무한 모드에서 획득한 젤리 코인을 생선으로 환산
-// 환산 : 젤리 1코인 = 생선 1마리
+// 플레이 점수
+// 보상으로 젤리 발바닥 1개당 점수 100점
+// 젤리 발바닥 1개당 초밥 5개
 public class SoleJelly : Goods
 {
 	private void Awake()
