dd4313c0f4177f02be1d2433b93a8f4877a3b1bd
KANG SOO IN <ks00in213@gmail.com>
2024-08-07 03:30:15 +0900

    feat : Add WaitTask

--- Assets/KSI/Scripts/Managers/BackendManager.cs
+++ Assets/KSI/Scripts/Managers/BackendManager.cs
@@ -8,7 +8,7 @@ public class BackendManager : MonoBehaviour
 {
 	private void Start()
 	{
-		var bro = Backend.Initialize();
+		var bro = Backend.Initialize(true);
 
 		if (bro.IsSuccess())
 		{
@@ -19,66 +19,102 @@ public class BackendManager : MonoBehaviour
 			Debug.LogError("초기화 실패 : " + bro);
 		}
 
-		Test();
+		StartCoroutine(TestRoutine());
 	}
 
-	async void Test()
+	private IEnumerator TestRoutine()
 	{
-		await Task.Run(() => {
-			CustomLogin("강수인", "1234");
-			// UpdateNickname("LLucky Studio");
-			// TODO : 게임 정보 기능 구현 로직 추가
-			Debug.Log("테스트를 종료합니다.");
-		});
+		yield return StartCoroutine(CustomSignUpAsyncRoutine("kangsooin", "1234"));
 	}
 
 	public void CustomSignUp(string id, string pw)
 	{
-		Debug.Log("회원가입을 요청합니다.");
-
-		var bro = Backend.BMember.CustomSignUp(id, pw);
-
-		if (bro.IsSuccess())
-		{
-			Debug.Log("회원가입에 성공했습니다. : " + bro);
-		}
-		else
-		{
-			Debug.LogError("회원가입에 실패했습니다. : " + bro);
-		}
+		StartCoroutine(CustomSignUpAsyncRoutine(id, pw));
 	}
 
-	public void CustomLogin(string id, string pw)
+	// 비동기 방식으로 회원가입 요청 처리
+	IEnumerator CustomSignUpAsyncRoutine(string id, string pw)
 	{
-		Debug.Log("로그인을 요청합니다.");
-
-		var bro = Backend.BMember.CustomLogin(id, pw);
-
-		if (bro.IsSuccess())
+		// Task.Run을 사용하여 백그라운드 스레드에서 실행될 비동기 작업 시작
+		yield return new WaitTask(Task.Run(() =>
 		{
-			Debug.Log("로그인이 성공했습니다. : " + bro);
-		}
-		else
-		{
-			Debug.LogError("로그인이 실패했습니다. : " + bro);
-		}
+			Debug.Log("회원가입 요청합니다.");
+
+			// 뒤끝 서버 SDK를 이용한 사용자 정의 회원가입 요청
+			var bro = Backend.BMember.CustomSignUp(id, pw);
+
+			// 회원가입 요청 결과에 따른 처리
+			if (bro.IsSuccess())
+			{
+				Debug.Log("회원가입 성공했습니다. : " + bro);
+			}
+			else
+			{
+				Debug.LogError("회원가입 실패했습니다. : " + bro);
+			}
+		}));
 	}
 
-	public void UpdateNickname(string nickname)
-	{
-		Debug.Log("닉네임 변경을 요청합니다.");
+	//async void Test()
+	//{
+	//	await Task.Run(() => {
+	//		CustomLogin("강수인", "1234");
+	//		// UpdateNickname("LLucky Studio");
+	//		BackendGameData.Instance.GameDataInsert();
+	//		// TODO : 데이터 삽입 함수
+	//		Debug.Log("테스트를 종료합니다.");
+	//	});
+	//}
 
-		var bro = Backend.BMember.UpdateNickname(nickname);
 
-		if (bro.IsSuccess())
-		{
-			Debug.Log("닉네임 변경에 성공했습니다 : " + bro);
-		}
-		else
-		{
-			Debug.LogError("닉네임 변경에 실패했습니다 : " + bro);
-		}
-	}
+
+	//public void CustomSignUp(string id, string pw)
+	//{
+	//	Debug.Log("회원가입을 요청합니다.");
+
+	//	var bro = Backend.BMember.CustomSignUp(id, pw);
+
+	//	if (bro.IsSuccess())
+	//	{
+	//		Debug.Log("회원가입에 성공했습니다. : " + bro);
+	//	}
+	//	else
+	//	{
+	//		Debug.LogError("회원가입에 실패했습니다. : " + bro);
+	//	}
+	//}
+
+	//public void CustomLogin(string id, string pw)
+	//{
+	//	Debug.Log("로그인을 요청합니다.");
+
+	//	var bro = Backend.BMember.CustomLogin(id, pw);
+
+	//	if (bro.IsSuccess())
+	//	{
+	//		Debug.Log("로그인이 성공했습니다. : " + bro);
+	//	}
+	//	else
+	//	{
+	//		Debug.LogError("로그인이 실패했습니다. : " + bro);
+	//	}
+	//}
+
+	//public void UpdateNickname(string nickname)
+	//{
+	//	Debug.Log("닉네임 변경을 요청합니다.");
+
+	//	var bro = Backend.BMember.UpdateNickname(nickname);
+
+	//	if (bro.IsSuccess())
+	//	{
+	//		Debug.Log("닉네임 변경에 성공했습니다 : " + bro);
+	//	}
+	//	else
+	//	{
+	//		Debug.LogError("닉네임 변경에 실패했습니다 : " + bro);
+	//	}
+	//}
 }
 
 
