ab5c7b11e3c3ec9332308fcc26870779cb1fafbc
김석진 <69078017+eeptmxlsl15@users.noreply.github.com>
2024-08-13 04:33:41 +0900

    feat : rank system ui

--- Assets/KSJ/Scripts/Backend/Constants.cs
+++ Assets/KSJ/Scripts/Backend/Constants.cs
@@ -2,4 +2,5 @@ public static class Constants
 {
 	public static readonly string USER_DATA_TABLE = "USER_DATA";
 	public static readonly string INFINITE_RANK_UUID = "39da65b0-58af-11ef-b348-b51ad7d2680a";
+	public static readonly int MAX_RANK_LIST = 20;
 }
