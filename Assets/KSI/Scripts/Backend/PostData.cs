188432468e72318adad5544de7ec487569503645
KANG SOO IN <ks00in213@gmail.com>
2024-08-13 02:00:44 +0900

    feat : Add PostData

---
+++ Assets/KSI/Scripts/Backend/PostData.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostData : MonoBehaviour
+{
+	public string title; // 우편 제목
+	public string content; // 우편 내용
+	public string inDate; // 우편 inDate
+	public string expirationDate; // 우편 만료 날짜
+
+	public bool isCanReceive = false; // 우편에 받을 수 있는 아이템이 있는지 여부
+
+	// <아이템 이름, 아이템 개수>
+	public Dictionary<string, int> postReward = new Dictionary<string, int>();
+
+	public override string ToString()
+	{
+		string result = string.Empty;
+		result += $"title : {title}\n";
+		result += $"content : {content}\n";
+		result += $"inDate : {inDate}\n";
+
+		if (isCanReceive)
+		{
+			result += "우편 아이템\n";
+
+			foreach (string itemKey in postReward.Keys)
+			{
+				result += $"| {itemKey} : {postReward[itemKey]}개\n";
+			}
+		}
+		else
+		{
+			result += "지원하지 않는 아이템입니다.";
+		}
+
+		return result;
+	}
+}
